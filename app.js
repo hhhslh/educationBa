5633f0683101f63722dbafdc1b1ec77535c8e9a9
hhhslh <3064063267@qq,com>
2020-02-17 14:50:45 +0800

    0217 s

--- app.js
+++ app.js
@@ -5,33 +5,41 @@ App({
   },
   onShow() {
     this.messageNumber()
-    if (wx.canIUse("getUpdateManager")) {
+    //使用更新对象之前判断是否可用
+    if (wx.canIUse('getUpdateManager')) {
       console.log("检查更新")
       const updateManager = wx.getUpdateManager()
       updateManager.onCheckForUpdate(function (res) {
         // 请求完新版本信息的回调
-        console.log(res.hasUpdate)
+        console.log(res.hasUpdate)//res.hasUpdate返回boolean类型
+        if (res.hasUpdate) {
+          updateManager.onUpdateReady(function () {
+            wx.showModal({
+              title: '更新提示',
+              content: '新版本已经准备好，是否重启当前应用？',
+              success(res) {
+                if (res.confirm) {
+                  wx.clearStorage()
+                  // 新的版本已经下载好，调用applyUpdate应用新版本并重启
+                  updateManager.applyUpdate()
+                }
+              }
+            })
+          })
+          // 新版本下载失败时执行
+          updateManager.onUpdateFailed(function () {
+            wx.showModal({
+              title: '发现新版本',
+              content: '请删除当前小程序，重新搜索打开...',
+            })
+          })
+        }
       })
-      updateManager.onUpdateReady(function () {
-        wx.showModal({
-          title: '更新提示',
-          content: '2.14版本已经准备好，是否重启应用？',
-          success(res) {
-            console.log("正在更新")
-            if (res.confirm) {
-              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-              updateManager.applyUpdate()
-            }
-          }
-        })
-      })
-      updateManager.onUpdateFailed(function () {
-        console.log("更新失败")
-        // 新版本下载失败
-        wx.showModal({
-          title: '更新提示',
-          content: '2.14版本下载失败',
-        })
+    } else {
+      //如果小程序需要在最新的微信版本体验，如下提示
+      wx.showModal({
+        title: '更新提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
       })
     }
   },
